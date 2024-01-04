e799bd6750514d6b249888d2110b0dbb4cabb82e
JanAppAkademie <jan.biernacki@app-akademie.com>
2024-01-04 13:23:15 +0100

    Aufgabe 3

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 void main() {
-  runApp(MaterialApp(home: ExersiceSelection()));
+  runApp(const ExersiceSelection());
 }
 
 class ExersiceSelection extends StatelessWidget {
@@ -9,36 +10,71 @@ class ExersiceSelection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return MaterialApp(
+      routes: {
+        "exersice1" : (context) => const Exersice1(),
+          "exersice2" : (context) => const Exersice2(),
+            "exersice3" : (context) => const Exersice3(),
+      } , 
+        builder: (context, child) => ResponsiveBreakpoints.builder(
+              child: child!,
+              breakpoints: [
+                const Breakpoint(start: 0, end: 450, name: MOBILE),
+                const Breakpoint(start: 451, end: 800, name: TABLET),
+                const Breakpoint(start: 801, end: 1920, name: DESKTOP),
+                const Breakpoint(start: 1921, end: double.infinity, name: '4K'),
+              ],
+            ),
+        home: const LandingPage());
+
+
+        
+  }
+}
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context)
-                    .push(MaterialPageRoute(builder: (context) => const Exersice1()));
-              },
-              child: const Text("Exersice 1"),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                   Navigator.pushNamed(context, "exersice1");
+                  },
+                  child: const Text("Exersice 1"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                   Navigator.pushNamed(context, "exersice2");
+                  },
+                  child: const Text("Exersice 2"),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, "exersice3");
+                  },
+                  child: const Text("Exersice 3"),
+                ),
+              ],
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                    MaterialPageRoute(builder: (context) => const Exersice2()));
-              },
-              child: const Text("Exersice 2"),
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        );
   }
 }
 
 class Exersice1 extends StatelessWidget {
+
   const Exersice1({super.key});
 
   @override
@@ -49,50 +85,50 @@ class Exersice1 extends StatelessWidget {
       children: [
         SizedBox(height: size.height * 0.4),
         SizedBox(
-          width: size.width,
-          
-          child:
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            
-            Icon(
-              Icons.person,
-              size: size.height * 0.05,
-            ),
-            SizedBox(width: size.width * 0.02),
-            Flexible(child: 
-            Column(
-              children: [
-             Text(
-                    "Flutter McFlutter",
-                    style: TextStyle(fontSize: size.height * 0.028),
-                
-                
-                 ),
-                
-                 
-                Text("Experienced App Developer",
-                    style: TextStyle(fontSize: size.height * 0.020),),
-                 
-                 
-              ],
-            ),
-        ),
-            
-          ],
-        ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                Icons.person,
+                size: size.height * 0.05,
+              ),
+              SizedBox(width: size.width * 0.02),
+              Flexible(
+                child: Column(
+                  children: [
+                    Text(
+                      "Flutter McFlutter",
+                      style: TextStyle(fontSize: size.height * 0.028),
+                    ),
+                    Text(
+                      "Experienced App Developer",
+                      style: TextStyle(fontSize: size.height * 0.020),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
         SizedBox(height: size.height * 0.01),
         Padding(
           padding: EdgeInsets.symmetric(horizontal: size.height * 0.04),
-          child:  Row(
+          child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              Flexible(child:
-              Text("123 Main Street",
-                    style: TextStyle(fontSize: size.height * 0.02),),), Flexible(child: Text("(415) 555-0198",
-                    style: TextStyle(fontSize: size.height * 0.02),),)],
+              Flexible(
+                child: Text(
+                  "123 Main Street",
+                  style: TextStyle(fontSize: size.height * 0.02),
+                ),
+              ),
+              Flexible(
+                child: Text(
+                  "(415) 555-0198",
+                  style: TextStyle(fontSize: size.height * 0.02),
+                ),
+              )
+            ],
           ),
         ),
         SizedBox(height: size.height * 0.01),
@@ -100,12 +136,13 @@ class Exersice1 extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             Flexible(child: Icon(Icons.man)),
-            Flexible(child:    Icon(Icons.punch_clock),),
+            Flexible(
+              child: Icon(Icons.punch_clock),
+            ),
             Flexible(child: Icon(Icons.phone_android)),
             Flexible(child: Icon(Icons.phone_iphone)),
           ],
         ),
-       
       ],
     ));
   }
@@ -124,9 +161,6 @@ class Exersice2 extends StatelessWidget {
       itemBuilder: (context, index) {
         return Container(
           color: Colors.primaries[index % Colors.primaries.length],
-          margin: const EdgeInsets.all(8.0),
-          height: 100.0,
-          width: 100.0,
         );
       },
     );
@@ -136,10 +170,36 @@ class Exersice2 extends StatelessWidget {
     double screenWidth = MediaQuery.of(context).size.width;
 
     if (screenWidth > 600) {
-      return 4; 
+      return 4;
     } else {
-      return 2; 
+      return 2;
     }
   }
+}
 
+class Exersice3 extends StatelessWidget {
+  const Exersice3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: calculateBreakPoint(context),
+      ),
+      itemCount: 8,
+      itemBuilder: (context, index) {
+        return Container(
+          color: Colors.primaries[index % Colors.primaries.length],
+        );
+      },
+    );
+  }
+
+  int calculateBreakPoint(BuildContext context) {
+    if (ResponsiveBreakpoints.of(context).largerThan(MOBILE)) {
+      return 4;
+    } else {
+      return 2;
+    }
+  }
 }
